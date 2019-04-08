138fd4022d4da86b624e4bc94f2da3a3d63e16e6
Khensu-Ra Love El <khensura25@KhensuRasMBP2.home>
2019-04-07 23:16:53 -0400

    New files and Assets

---
+++ week1/example.js
@@ -0,0 +1,17 @@
+var request = require('request');
+var url = process.argv[2];
+
+// process.argv.forEach(function (val, index, array) {
+//     console.log(index + ': ' + val);
+//   });
+// for (let j = 0; j < process.argv.length; j++) {  
+// //     console.log(j + ' -> ' + (process.argv[j]));
+// }
+request(url, function (err, response, body){
+    console.log('error: ', err); //prints error if one occurs
+    console.log('statusCode:', response && response.statusCode); //prints response and status code
+    console.log('body:', body); //Prints HTML for ESPN page;t
+
+
+})
+
