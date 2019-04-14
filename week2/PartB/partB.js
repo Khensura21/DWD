a992cb842c9fa980c8eed7b1d4b39ec1284644d9
Khensu-Ra Love El <khensura25@10-17-78-142.dynapool.wireless.nyu.edu>
2019-04-14 16:34:04 -0400

    Post Method

--- week2/PartB/partB.js
+++ week2/PartB/partB.js
@@ -1,10 +1,16 @@
 var express = require("express");
 var app = express();
 
+var bodyParser = require('body-parser');
+
+var urlencodedParser = bodyParser.urlencoded({ extended: true }); // for parsing form data
+
+app.use(urlencodedParser); 
+
 app.use(express.static('public'));
 
-app.get('/band', function(req, res) {
-    var textvalue = req.query.textfield;
+app.post('/processit', function(req, res) {
+    var textvalue = req.body.textfield;
 
     let insults = ["ugly", "dumb", "lame"];
     var insult = insults[Math.floor(Math.random()*insults.length)];
@@ -16,4 +22,6 @@ app.get('/band', function(req, res) {
 app.listen(3550, function(){
     console.log("App listening on port 3550!")
 })
+
+
 
